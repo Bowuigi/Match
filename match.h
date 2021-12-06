c82178d37f369cdeda93c32d9acb2925f71d63a9
Bowuigi <matucrimella@gmail.com>
2021-12-06 17:10:12 -0300

    Added license to mgr (zlib) and made the licenses for the library a bit prettier

--- match.h
+++ match.h
@@ -1,56 +1,26 @@
 /*
-Match: Simple (and tiny) string matching library
+	Match: Simple (and tiny) string matching library
 
+	(C) 2021 Bowuigi
 
-(C) 2021 Bowuigi
+	zlib license
+	------------
 
-zlib license
-------------
+	This software is provided 'as-is', without any express or implied
+	warranty.  In no event will the authors be held liable for any damages
+	arising from the use of this software.
 
-This software is provided 'as-is', without any express or implied
-warranty.  In no event will the authors be held liable for any damages
-arising from the use of this software.
+	Permission is granted to anyone to use this software for any purpose,
+	including commercial applications, and to alter it and redistribute it
+	freely, subject to the following restrictions:
 
-Permission is granted to anyone to use this software for any purpose,
-including commercial applications, and to alter it and redistribute it
-freely, subject to the following restrictions:
-
-1. The origin of this software must not be misrepresented; you must not
-claim that you wrote the original software. If you use this software
-in a product, an acknowledgment in the product documentation would be
-appreciated but is not required.
-2. Altered source versions must be plainly marked as such, and must not be
-misrepresented as being the original software.
-3. This notice may not be removed or altered from any source distribution.
-
-Description
------------
-
-Uses syntax similar to the Lua one, the options are the following:
-
-+-------+--------------+
-| Token |   Matches    |
-|-------+--------------|
-|  %a   |   Anything   |
-|  %h   | Alphanumeric |
-|  %l   |    Letters   |
-|  %n   |    Numbers   |
-|  %p   |  Punctuation |
-|  %s   |    Spaces    |
-|  %u   |   Uppercase  |
-|  %x   |   Hex [0-F]  |
-+-------+--------------+
-
-If the tokens are in uppercase, then that match would be inverted, for example, %n matches numbers, but %N matches anything but numbers.
-
-If the token doesn't start with % then it is just a normal exact match
-
-If the token is %%, then it matches '%'
-
-Usage
------
-
-Although there is a lot of functions here, the only one that you might want to use is Match_String, since the other ones are for expanding the library
+	1. The origin of this software must not be misrepresented; you must not
+	claim that you wrote the original software. If you use this software
+	in a product, an acknowledgment in the product documentation would be
+	appreciated but is not required.
+	2. Altered source versions must be plainly marked as such, and must not be
+	misrepresented as being the original software.
+	3. This notice may not be removed or altered from any source distribution.
 */
 
 #ifndef MATCH_H
